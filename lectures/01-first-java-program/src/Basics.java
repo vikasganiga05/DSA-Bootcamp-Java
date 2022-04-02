f0909da5344812f930f3454957627adddd04550c
Vikas Ganiga <codewithvikas@gmail.com>
2022-04-03 01:59:16 +0530

    feat: Add basic conditions and loops

---
+++ lectures/01-first-java-program/src/Basics.java
@@ -0,0 +1,21 @@
+public class Basics {
+  public static void main(String[] args) {
+    /* Conditional Statements */
+    // int a = 10;
+    // if (a == 10) {
+    //   System.out.print("Hello World");
+    // }
+
+    /* While Loop */
+    // int count = 1;
+    // while (count != 5) {
+    //   System.out.println(count);
+    //   count++;
+    // }
+
+    /* For Loop */
+    for (int count = 1; count != 5; count++) {
+      System.out.println(count);
+    }
+  }
+}
