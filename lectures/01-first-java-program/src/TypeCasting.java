816d89fbb52d33057f048d2e0a9b8ff98b8b6aaa
Vikas Ganiga <codewithvikas@gmail.com>
2022-04-03 02:30:39 +0530

    fix: Fix format of TypeCasting class

--- lectures/01-first-java-program/src/TypeCasting.java
+++ lectures/01-first-java-program/src/TypeCasting.java
@@ -35,15 +35,15 @@ public class TypeCasting {
           1. All the byte, short and charecter values are promoted to integer.
           2. If any one of the operands is long the whole operation will be promoted to long.
     */
-   byte b = 42;
-   char c = 'a';
-   short s = 1024;
-   int i = 50000;
-   float f = 5.67F;
-   double d = 0.1234;
-   double result = (f * b) + (i / c) - (d * s);
-   // float + int - double = double
-   System.out.println((f * b) + " " + (i / c) + " " + (d * s));
-   System.out.println(result);
+    byte b = 42;
+    char c = 'a';
+    short s = 1024;
+    int i = 50000;
+    float f = 5.67F;
+    double d = 0.1234;
+    double result = (f * b) + (i / c) - (d * s);
+    // float + int - double = double
+    System.out.println((f * b) + " " + (i / c) + " " + (d * s));
+    System.out.println(result);
   }
 }
