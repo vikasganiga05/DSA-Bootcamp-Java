bd299a1919e76f2132c1014a1b750b4e92d8ec68
Vikas Ganiga <codewithvikas@gmail.com>
2022-04-02 23:13:54 +0530

    feat: Update Primitives class

--- lectures/01-first-java-program/src/Primitives.java
+++ lectures/01-first-java-program/src/Primitives.java
@@ -6,5 +6,12 @@ public class Primitives {
     double largeDecimal = 4567654.4567;
     long largeInteger = 3456734567876543L;
     boolean check = true;
+
+    System.out.println("Roll No : " + rollno);
+    System.out.println("Letter : " + letter);
+    System.out.println("Mark : " + mark);
+    System.out.println("Large Decimal : " + largeDecimal);
+    System.out.println("Large Integer : " + largeInteger);
+    System.out.println("Check : " + check);
   }
 }
