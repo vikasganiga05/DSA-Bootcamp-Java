0e2cfb8f5ead3932604fb105f9f2bb5ccff8210b
Vikas Ganiga <codewithvikas@gmail.com>
2022-04-02 23:06:47 +0530

    feat: Add primitive data-types

---
+++ lectures/01-first-java-program/src/Primitives.java
@@ -0,0 +1,10 @@
+public class Primitives {
+  public static void main(String[] args) {
+    int rollno = 1015;
+    char letter = 'V';
+    float mark = 98.67F;
+    double largeDecimal = 4567654.4567;
+    long largeInteger = 3456734567876543L;
+    boolean check = true;
+  }
+}
