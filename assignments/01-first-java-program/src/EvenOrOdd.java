ce4d53bb4669a952fc90263ca7434b32b19276e8
Vikas Ganiga <codewithvikas@gmail.com>
2022-04-03 18:09:43 +0530

    feat: Add solution for even or odd number

---
+++ assignments/01-first-java-program/src/EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Please enter your number: ");
+    int number = input.nextInt();
+    if (number % 2 == 0) {
+      System.out.println("The number " + number + " is Even.");
+      return;
+    } else {
+      System.out.println("The number " + number + " is Odd.");
+      return;
+    }
+  }
+}
