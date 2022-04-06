2a3e667c0d6a159f469f65ab412fd7e8c67bdabd
Vikas Ganiga <codewithvikas@gmail.com>
2022-04-06 08:41:01 +0530

    feat: Add solution for Fibonacci Series up to n numbers

---
+++ assignments/01-first-java-program/src/FibonacciSeries.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n, i, next, f1 = 0, f2 = 1;
+    System.out.print("Please enter the nth value: ");
+    n = input.nextInt();
+    input.close();
+
+    if (n < 1) {
+      return;
+    }
+
+    System.out.printf("First %d fibonacci numbers are: ", n);
+    System.out.print(f1 + " ");
+    for (i = 1; i < n; i++) {
+      System.out.print(f2 + " ");
+      next = f1 + f2;
+      f1 = f2;
+      f2 = next;
+    }
+  }
+}
